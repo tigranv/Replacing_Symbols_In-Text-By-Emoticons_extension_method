826f1e0342b4c094ad5f1a4821bb63b45b7f98c4
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-17 02:55:32 +0400

    core

---
+++ EmoticonsConverter/EmoticonsConverter/ConvertEmoticonsExtention.cs
@@ -0,0 +1,37 @@
+﻿namespace EmoticonsConverter
+{
+    public static class ConvertEmoticonsExtention
+    {
+        // Extention method 
+        public static string ConvertEmoticons(this string value)
+        {
+            string NewValue = string.Empty;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (i != value.Length - 1)
+                {
+                    switch (value.Substring(i, 2))
+                    {
+                        case ":)":
+                            NewValue += "\u263B";
+                            i++;
+                            break;
+                        case ":(":
+                            NewValue += "\u2639";
+                            i++;
+                            break;
+                        default:
+                            NewValue += value[i].ToString();
+                            break;
+                    }
+                }
+                else
+                {
+                    NewValue += value[i].ToString();
+                    break;
+                }
+            }
+            return NewValue;
+        }
+    }
+}
---
+++ EmoticonsConverter/TestExtention/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EmoticonsConverter;
+
+namespace TestExtention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter eny text");
+            string st = Console.ReadLine();
+            Console.WriteLine(new string('-', 30));
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(st.ConvertEmoticons());
+
+            Console.ReadKey();
+        }
+    }
+}
